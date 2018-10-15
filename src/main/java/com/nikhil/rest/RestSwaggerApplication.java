f2c5d7c2a5fd025a52e6ead44499fb244fc8449c
nikhil-raj <nikhil.raj@allianz.com>
2018-10-15 13:16:20 +0200

    Feature complete

--- src/main/java/com/nikhil/rest/RestSwaggerApplication.java
+++ src/main/java/com/nikhil/rest/RestSwaggerApplication.java
@@ -1,4 +1,4 @@
-package com.nikhil.rest.restswagger;
+package com.nikhil.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/nikhil/rest/db/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.nikhil.rest.db;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<PersonEntity, Integer> {
+
+}
